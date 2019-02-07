c60ec6ee6c9d3779e484028aecfdd0d239deb6bd
Gabriel Tosta <gabriel.tosta90@gmail.com>
2019-02-07 09:48:37 -0200

    Inserinfo uma nova Categoria com POST

--- src/main/java/com/tosta/cursomc/resources/CategoriaResource.java
+++ src/main/java/com/tosta/cursomc/resources/CategoriaResource.java
@@ -1,11 +1,15 @@
 package com.tosta.cursomc.resources;
 
+import java.net.URI;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.tosta.cursomc.domain.Categoria;
 import com.tosta.cursomc.services.CategoriaService;
@@ -26,6 +30,14 @@ public class CategoriaResource {
 		Categoria obj = service.buscar(id);
 		
 		return ResponseEntity.ok(obj);
+	}
+	
+	@RequestMapping(method=RequestMethod.POST)
+	public ResponseEntity<Void> insert(@RequestBody Categoria obj) throws ObjectNotFoundException {
+		
+		obj = service.insert(obj);
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(obj.getId()).toUri();
+		return ResponseEntity.created(uri).build();
 	}
 
 }
