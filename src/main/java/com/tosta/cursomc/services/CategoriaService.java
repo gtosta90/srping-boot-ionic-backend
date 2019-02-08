af74746d44c29fcd567eed2421d8726369c9c3ba
Gabriel Tosta <gabriel.tosta90@gmail.com>
2019-02-08 16:01:09 -0200

    Validacao sintatica com BeanValidation

--- src/main/java/com/tosta/cursomc/services/CategoriaService.java
+++ src/main/java/com/tosta/cursomc/services/CategoriaService.java
@@ -11,6 +11,7 @@ import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
 
 import com.tosta.cursomc.domain.Categoria;
+import com.tosta.cursomc.dto.CategoriaDTO;
 import com.tosta.cursomc.repository.CategoriaRepository;
 import com.tosta.cursomc.services.exceptions.DataIntegrityException;
 import com.tosta.cursomc.services.exceptions.ObjectNotFoundException;
@@ -59,6 +60,10 @@ public class CategoriaService {
 		PageRequest pageRequest = PageRequest.of(page, linesPerPage, Direction.valueOf(direction), orderBy);
 		
 		return repo.findAll(pageRequest);
+	}
+	
+	public Categoria fromDTO(CategoriaDTO objDto) {
+		return new Categoria(objDto.getId(), objDto.getNome());
 	}
 	
 	
