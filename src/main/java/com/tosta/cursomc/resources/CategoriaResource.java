f5a596e6220e89e62f12a16fa21c4ab22da00662
Gabriel Tosta <gabriel.tosta90@gmail.com>
2019-02-08 15:37:31 -0200

    Paginação com parametros opcionais

--- src/main/java/com/tosta/cursomc/resources/CategoriaResource.java
+++ src/main/java/com/tosta/cursomc/resources/CategoriaResource.java
@@ -5,11 +5,13 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
@@ -62,6 +64,18 @@ public class CategoriaResource {
 		
 		List<Categoria> list = service.findAll();
 		List<CategoriaDTO> listDto = list.stream().map(obj -> new CategoriaDTO(obj)).collect(Collectors.toList());
+		return ResponseEntity.ok().body(listDto);
+	}
+	
+	@RequestMapping(value="/page", method=RequestMethod.GET)
+	public ResponseEntity<Page<CategoriaDTO>> findPage(
+			@RequestParam(value="page", defaultValue="0") String page, 
+			@RequestParam(value="linesPerPage", defaultValue="24")String linesPerPage, 
+			@RequestParam(value="orderBy", defaultValue="nome")String orderBy, 
+			@RequestParam(value="direction", defaultValue="ASC")String direction) throws ObjectNotFoundException {
+		
+		Page<Categoria> pageReturn = service.findPage(Integer.parseInt(page), Integer.parseInt(linesPerPage), orderBy, direction);
+		Page<CategoriaDTO> listDto = pageReturn.map(obj -> new CategoriaDTO(obj));
 		return ResponseEntity.ok().body(listDto);
 	}
 
